3e165fa9ecf525704c1c60656cd236f2190d7d6e
Jaseem-c <seamjaseem@gmail.com>
2024-08-19 08:44:03 +0530

    url added

--- src/services/pro_serverUrl.js
+++ src/services/pro_serverUrl.js
@@ -1,1 +1,1 @@
-export const serverUrl = 'http://localhost:3000' // Base URL of the backend server
+export const serverUrl = 'https://prochauffeurs-backend.onrender.com' // Base URL of the backend server
